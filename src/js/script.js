2606eb43f76080f45542323bfb10453634ab8e51
DSStepanov <DSStepanov@lanit.ru>
2020-08-07 14:02:36 +0500

    Доделал верстку + начало анимирования

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
   function setSizes() {
-    setTimeout(function() {
+    setTimeout(function () {
       // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
       let vh = window.innerHeight * 0.01;
       // Then we set the value in the --vh custom property to the root of the document
@@ -11,80 +11,175 @@ $(function() {
 
   setSizes();
 
-  $(window).resize(function() {
+  $(window).resize(function () {
     setSizes();
   });
 
-  class Animal {
-    constructor(name) {
-      this.name = name;
-    }
+  new WOW().init();
 
-    walk() {
-      console.log(`${this.name} идет`);
-    }
+  var chart1 = document.getElementById("chart1");
+  var chart2 = document.getElementById("chart2");
+  var chart3 = document.getElementById("chart3");
+  var chart4 = document.getElementById("chart4");
 
-    static say() {
-      console.log("Я животное");
-    }
-  }
-  var cat = new Animal("Ева");
-
-  /* ============================== */
-  // var button = document.querySelector("button");
-
-  // window.addEventListener("resize", function() {
-  //   console.log("resize");
-  // });
-
-  // console.dir(document.constructor.name);
-
-  // console.dir(h1.offsetTop);
-  // console.dir(document.documentElement.scrollTop);
-  // console.dir(window.pageYOffset);
-  // console.dir(document.body.clientWidth);
-  // console.dir(document.body.offsetWidth);
-  // console.dir(window.innerWidth);
-
-  // let ball = document.getElementById("ball");
-  // let box = document.getElementById("box");
-
-  // ball.onmousedown = function(event) {
-  //   ball.style.position = "absolute";
-
-  //   console.log(box.getBoundingClientRect(), event);
-
-  //   ball.addEventListener("mousemove", moveAt);
-
-  //   function moveAt(event) {
-  //     // ball.style.left = event.clientX - box.getBoundingClientRect().left + "px";
-  //     // ball.style.top = event.clientY - box.getBoundingClientRect().top + "px";
-  //   }
-
-  //   ball.onmouseup = function(event) {
-  //     ball.style.position = "";
-  //     ball.removeEventListener("mousemove", moveAt);
-  //   };
-  // };
-
-  // let progress = document.querySelector(".progress");
-
-  // window.addEventListener("scroll", progressBar);
-
-  // function progressBar(event) {
-  //   let winScroll =
-  //     document.body.scrollTop || document.documentElement.scrollTop;
-  //   let winHeight =
-  //     document.documentElement.scrollHeight -
-  //     document.documentElement.clientHeight;
-
-  //   progress.style.width = (winScroll / winHeight) * 100 + "%";
-  // }
-
-  document.addEventListener("keydown", event => {
-    document.addEventListener("keyup", event => {
-      console.log(event);
-    });
+  var myChart1 = new Chart(chart1, {
+    type: "doughnut",
+    data: {
+      labels: ["Физ. лица", "Юр. лица", "ИП"],
+      datasets: [
+        {
+          label: "Pints",
+          data: [705894, 95980, 2474],
+          backgroundColor: [
+            "#2999d6",
+            "#7bbee2",
+            "#29c8d6",
+            "#0ac883",
+            "#dac42f",
+            "#46a06a",
+          ],
+        },
+      ],
+    },
+    options: {
+      cutoutPercentage: 75,
+      legend: {
+        display: false,
+      },
+    },
+    plugins: {
+      deferred: {
+        // enabled by default
+        xOffset: 150, // defer until 150px of the canvas width are inside the viewport
+        yOffset: "50%", // defer until 50% of the canvas height are inside the viewport
+        delay: 500, // delay of 500 ms after the canvas is considered inside the viewport
+      },
+    },
   });
-  
+
+  var myChart2 = new Chart(chart2, {
+    type: "bar",
+    data: {
+      labels: ["", "", "", "", ""],
+      datasets: [
+        {
+          data: [716, 416, 316, 308, 308],
+          backgroundColor: [
+            "#2999d6",
+            "#7bbee2",
+            "#29c8d6",
+            "#0ac883",
+            "#dac42f",
+            "#46a06a",
+          ],
+        },
+      ],
+    },
+    options: {
+      cutoutPercentage: 75,
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: { display: false },
+            display: false,
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: { display: false },
+            display: false,
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  var myChart3 = new Chart(chart3, {
+    type: "bar",
+    data: {
+      labels: ["", "", "", "", ""],
+      datasets: [
+        {
+          data: [654, 356, 302, 298, 290],
+          backgroundColor: [
+            "#2999d6",
+            "#7bbee2",
+            "#29c8d6",
+            "#0ac883",
+            "#dac42f",
+            "#46a06a",
+          ],
+        },
+      ],
+    },
+    options: {
+      cutoutPercentage: 75,
+      legend: {
+        display: false,
+      },
+      scales: {
+        xAxes: [
+          {
+            gridLines: { display: false },
+            display: false,
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+        yAxes: [
+          {
+            gridLines: { display: false },
+            display: false,
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  });
+
+  var myChart4 = new Chart(chart4, {
+    type: "doughnut",
+    data: {
+      labels: [
+        "Федеральный орган исполнительной власти",
+        "Территориальный орган федерального органа исполнительной власти",
+        "Региональный орган исполнительной власти",
+        "Муниципальный орган власти",
+        "Крестьянское (фермерское) хозяйство",
+        "Иная организация",
+      ],
+      datasets: [
+        {
+          label: "Pints",
+          data: [716, 416, 316, 308, 308, 716],
+          backgroundColor: [
+            "#2999d6",
+            "#7bbee2",
+            "#29c8d6",
+            "#0ac883",
+            "#dac42f",
+            "#46a06a",
+          ],
+        },
+      ],
+    },
+    options: {
+      cutoutPercentage: 75,
+      legend: {
+        display: false,
+      },
+    },
+  });
 });
