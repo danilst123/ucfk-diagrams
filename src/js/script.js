3255ecdd1b6016441cb737eb59f032e336595020
DSStepanov <DSStepanov@lanit.ru>
2020-08-07 22:40:33 +0500

    перевел на Vue

--- src/js/script.js
+++ src/js/script.js
@@ -1,183 +1,5 @@
 $(function () {
-  new WOW().init();
-
-  var chart1 = document.getElementById("chart1");
-  var chart2 = document.getElementById("chart2");
-  var chart3 = document.getElementById("chart3");
-  var chart4 = document.getElementById("chart4");
-
-  setTimeout(function () {
-    new Chart(chart1, {
-      type: "doughnut",
-      data: {
-        labels: ["Физ. лица", "Юр. лица", "ИП"],
-        datasets: [
-          {
-            label: "Pints",
-            data: [705894, 95980, 2474],
-            backgroundColor: [
-              "#2999d6",
-              "#7bbee2",
-              "#29c8d6",
-              "#0ac883",
-              "#dac42f",
-              "#46a06a",
-            ],
-          },
-        ],
-      },
-      options: {
-        cutoutPercentage: 75,
-        legend: {
-          display: false,
-        },
-      },
-      plugins: {
-        deferred: {
-          // enabled by default
-          xOffset: 150, // defer until 150px of the canvas width are inside the viewport
-          yOffset: "50%", // defer until 50% of the canvas height are inside the viewport
-          delay: 2500, // delay of 500 ms after the canvas is considered inside the viewport
-        },
-      },
-    });
-
-    new Chart(chart2, {
-      type: "bar",
-      data: {
-        labels: ["", "", "", "", ""],
-        datasets: [
-          {
-            data: [716, 416, 316, 308, 308],
-            backgroundColor: [
-              "#2999d6",
-              "#7bbee2",
-              "#29c8d6",
-              "#0ac883",
-              "#dac42f",
-              "#46a06a",
-            ],
-          },
-        ],
-      },
-      options: {
-        cutoutPercentage: 75,
-        legend: {
-          display: false,
-        },
-        scales: {
-          xAxes: [
-            {
-              gridLines: { display: false },
-              display: false,
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              gridLines: { display: false },
-              display: false,
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
-
-    new Chart(chart3, {
-      type: "bar",
-      data: {
-        labels: ["", "", "", "", ""],
-        datasets: [
-          {
-            data: [654, 356, 302, 298, 290],
-            backgroundColor: [
-              "#2999d6",
-              "#7bbee2",
-              "#29c8d6",
-              "#0ac883",
-              "#dac42f",
-              "#46a06a",
-            ],
-          },
-        ],
-      },
-      options: {
-        cutoutPercentage: 75,
-        legend: {
-          display: false,
-        },
-        scales: {
-          xAxes: [
-            {
-              gridLines: { display: false },
-              display: false,
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-          yAxes: [
-            {
-              gridLines: { display: false },
-              display: false,
-              ticks: {
-                beginAtZero: true,
-              },
-            },
-          ],
-        },
-      },
-    });
-
-    new Chart(chart4, {
-      type: "doughnut",
-      data: {
-        labels: [
-          "Федеральный орган исполнительной власти",
-          "Территориальный орган федерального органа исполнительной власти",
-          "Региональный орган исполнительной власти",
-          "Муниципальный орган власти",
-          "Крестьянское (фермерское) хозяйство",
-          "Иная организация",
-        ],
-        datasets: [
-          {
-            label: "Pints",
-            data: [716, 416, 316, 308, 308, 716],
-            backgroundColor: [
-              "#2999d6",
-              "#7bbee2",
-              "#29c8d6",
-              "#0ac883",
-              "#dac42f",
-              "#46a06a",
-            ],
-          },
-        ],
-      },
-      options: {
-        cutoutPercentage: 75,
-        legend: {
-          display: false,
-        },
-      },
-      plugins: {
-        deferred: {
-          // enabled by default
-          xOffset: 150, // defer until 150px of the canvas width are inside the viewport
-          yOffset: "50%", // defer until 50% of the canvas height are inside the viewport
-          delay: 20500, // delay of 500 ms after the canvas is considered inside the viewport
-        },
-      },
-    });
-  }, 2000);
-
-  var options = {
+  var counterOptions = {
     useEasing: true,
     useGrouping: true,
     separator: " ",
@@ -186,12 +8,262 @@ $(function () {
     suffix: "",
   };
 
-  $("[data-counter]").each(function () {
-    var num = parseInt($(this).data("counter")); //end count
-    var counts = new CountUp(this, 0, num, null, 1, options);
+  var barOptions = {
+    cutoutPercentage: 75,
+    legend: {
+      display: false,
+    },
+    scales: {
+      xAxes: [
+        {
+          gridLines: { display: false },
+          display: false,
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+      yAxes: [
+        {
+          gridLines: { display: false },
+          display: false,
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
 
-    setTimeout(function () {
-      counts.start();
-    }, 1900);
+  var doughnutOptions = {
+    cutoutPercentage: 75,
+    legend: {
+      display: false,
+    },
+  };
+
+  var cards = [
+    {
+      title: "Количество \n действующих сертификатов",
+      icon: "../img/medal.svg",
+      infoList: [
+        {
+          title: "Физ. лица",
+          count: 705894,
+        },
+        {
+          title: "Юр. лица",
+          count: 123,
+        },
+        {
+          title: "ИП",
+          count: 123,
+        },
+      ],
+      diagram: {
+        type: "doughnut",
+        data: {
+          labels: ["Физ. лица", "Юр. лица", "ИП"],
+          datasets: [
+            {
+              label: "Pints",
+              data: [705894, 95980, 2474],
+              backgroundColor: [
+                "#2999d6",
+                "#7bbee2",
+                "#29c8d6",
+                "#0ac883",
+                "#dac42f",
+                "#46a06a",
+              ],
+            },
+          ],
+        },
+        options: doughnutOptions,
+      },
+      additionalCard: {
+        title: "Сертификаты, \n созданные в 2020 году",
+        icon: "../img/medal.svg",
+        infoList: [
+          {
+            title: "Физ. лица",
+            count: 705894,
+          },
+          {
+            title: "Юр. лица",
+            count: 123,
+          },
+          {
+            title: "ИП",
+            count: 123,
+          },
+        ],
+      },
+    },
+    {
+      title: "Запросы на сертификаты, созданные за 24 часа",
+      icon: "../img/medal.svg",
+      subtitle:
+        "Рейтинг 5-ти ТОФК, в которые были поданы запросы на создание сертификатов",
+      infoList: [
+        {
+          title: "Физ. лица",
+          count: 7894,
+        },
+        {
+          title: "Юр. лица",
+          count: 123,
+        },
+        {
+          title: "ИП",
+          count: 123,
+        },
+      ],
+      diagram: {
+        type: "bar",
+        data: {
+          labels: ["", "", "", "", ""],
+          datasets: [
+            {
+              data: [716, 416, 316, 308, 308],
+              backgroundColor: [
+                "#2999d6",
+                "#7bbee2",
+                "#29c8d6",
+                "#0ac883",
+                "#dac42f",
+                "#46a06a",
+              ],
+            },
+          ],
+        },
+        options: barOptions,
+      },
+    },
+    {
+      title: "Сертификаты, созданные за 24 часа",
+      icon: "../img/medal.svg",
+      subtitle:
+        "Рейтинг 5-ти ТОФК, в которые были поданы запросы на создание сертификатов",
+      infoList: [
+        {
+          title: "Физ. лица",
+          count: 123,
+        },
+        {
+          title: "Юр. лица",
+          count: 123,
+        },
+        {
+          title: "ИП",
+          count: 123,
+        },
+      ],
+      diagram: {
+        type: "bar",
+        data: {
+          labels: ["", "", "", "", ""],
+          datasets: [
+            {
+              data: [654, 356, 302, 298, 290],
+              backgroundColor: [
+                "#2999d6",
+                "#7bbee2",
+                "#29c8d6",
+                "#0ac883",
+                "#dac42f",
+                "#46a06a",
+              ],
+            },
+          ],
+        },
+        options: barOptions,
+      },
+    },
+    {
+      title: "Количество зарегистрированных организаций",
+      icon: "../img/house.svg",
+      infoList: [
+        {
+          title: "Физ. лица",
+          count: 12314,
+        },
+        {
+          title: "Юр. лица",
+          count: 123,
+        },
+        {
+          title: "ИП",
+          count: 123,
+        },
+      ],
+      diagram: {
+        type: "doughnut",
+        data: {
+          labels: [
+            "Федеральный орган исполнительной власти",
+            "Территориальный орган федерального органа исполнительной власти",
+            "Региональный орган исполнительной власти",
+            "Муниципальный орган власти",
+            "Крестьянское (фермерское) хозяйство",
+            "Иная организация",
+          ],
+          datasets: [
+            {
+              label: "Pints",
+              data: [716, 416, 316, 308, 308, 716],
+              backgroundColor: [
+                "#2999d6",
+                "#7bbee2",
+                "#29c8d6",
+                "#0ac883",
+                "#dac42f",
+                "#46a06a",
+              ],
+            },
+          ],
+        },
+        options: doughnutOptions,
+      },
+    },
+  ];
+
+  var app = new Vue({
+    el: "#app",
+    data: {
+      cards: cards,
+    },
+    mounted: function () {
+      new WOW().init();
+
+      var canvases = this.$refs.canvas;
+
+      setTimeout(function () {
+        for (var index = 0; index < cards.length; index++) {
+          new Chart(canvases[index], cards[index].diagram);
+        }
+      }, 1900);
+
+      $("[data-counter]").each(function () {
+        var num = parseInt($(this).data("counter")); //end count
+        var counts = new CountUp(this, 0, num, null, 1, counterOptions);
+
+        setTimeout(function () {
+          counts.start();
+        }, 1900);
+      });
+    },
+    methods: {
+      totalCount: function (card) {
+        var arr = card.infoList;
+        var sum = 0;
+
+        for (var index = 0; index < arr.length; index++) {
+          sum += arr[index].count;
+        }
+
+        return sum;
+      },
+    },
   });
 });
